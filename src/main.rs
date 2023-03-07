209be69a2901adf998e67e5346a6197db38fa694
nicholves <80966961+nicholves@users.noreply.github.com>
2023-03-07 14:57:50 -0500

    merged with command line argument taking

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 pub mod compressor;
 
+
 fn main() {
     let output = compressor::compress_contents_from_string("Hello my name is Oleg Ivanovich Veselskiy");
 
     println!("{}", output);
+
+    /*let mut decompress = false;
+    let mut compress = false;
+    let mut output_file = String::new();
+
+    let args: Vec<_> = env::args().collect();
+
+    if !(args.iter().find(|e| **e == "-compress".to_string())).is_none() {
+                compress = true;
+                println!("Compressing");
+    }
+
+    for (index, arg) in enumerate(&args) {
+        if *arg == "-o".to_string() {
+            output_file = args[index + 1];
+            println!("setting output file");
+        }
+        
+    }*/
 }
